70a18b904053e0581b6a5ef95f1e23073215fed0
Ilya02021983 <63600627+Ilya02021983@users.noreply.github.com>
2021-06-06 17:33:09 +0300

    Update test_say_hello.c

--- test/test_say_hello.c
+++ test/test_say_hello.c
@@ -2,25 +2,27 @@
 #include "say_hello.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> 
+#include <string.h>
 
-int main() {
-char* check_word = "everyone";
-char* hello_pre_str = "Hello ";
-char* hello_str = (char*) malloc(strlen(hello_pre_str) + strlen(check_word) + 1);
-strcpy(hello_str, hello_pre_str);
-char* assert_str; 
-char* comp_str; 
-assert_str = strcat(hello_str, check_word);
-comp_str = say_hello(check_word);
-if (strcmp(assert_str, comp_str) == 0)
+int main(int argc, char* argv[])
 {
- printf("<Unit test name 1: test result SUCCESS/>\n");
- free(hello_str);
- return 0; 
-}
-else {
- printf("<Unit test name 1: test result FAILED/>\n");
- return 1;
-}
+    char* check_word = "everyone";
+    char* hello_pre_str = "Hello ";
+    char* hello_str = (char*) malloc(strlen(hello_pre_str) + strlen(check_word) + 1);
+    strcpy(hello_str, hello_pre_str);
+    char* assert_str;
+    char* comp_str;
+    assert_str = strcat(hello_str, check_word);
+    comp_str = say_hello(check_word);
+    if (strcmp(assert_str, comp_str) == 0)
+    {
+        printf("<Unit test name %s: test result SUCCESS/>\n", argv[1]);
+        free(hello_str);
+        return 0;
+    }
+    else
+    {
+        printf("<Unit test name 1: test result FAILED\\>\n");
+        return 1;
+    }
 }
