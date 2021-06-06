db89f0b64c32536bdf69312f7b9cc1cce9289e43
Ilya02021983 <63600627+Ilya02021983@users.noreply.github.com>
2021-06-06 17:16:26 +0300

    Update test_say_hello.c

--- test/test_say_hello.c
+++ test/test_say_hello.c
@@ -6,7 +6,7 @@
 
 int main() {
 char* check_word = "everyone";
-char* hello_pre_str = "Hesllo ";
+char* hello_pre_str = "Hello ";
 char* hello_str = (char*) malloc(strlen(hello_pre_str) + strlen(check_word) + 1);
 strcpy(hello_str, hello_pre_str);
 char* assert_str; 
